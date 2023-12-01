d0198519b5755b1a90f08f01fd2898f9ec71dbb7
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 09:13:12 +0530

    Update DonorList.py

--- pages/DonorList.py
+++ pages/DonorList.py
@@ -51,4 +51,12 @@ if(grant != 'none'):
     required_columns = filtered_data[['title','voter_id', 'block_timestamp', 'token_symbol','amount','amountUSD', 'round_name']]
     st.dataframe(required_columns)
 
+    csv = convert_df(required_columns)
 
+    st.download_button(
+       "Press to Download",
+       csv,
+       "donors.csv",
+       "text/csv",
+       key='download-csv'
+    )
